a9b9dfd525fcd8da2cf9392103077638a76916af
Javier Hidalgo-Carrio <jhidalgocarrio@ieee.org>
2017-01-19 20:30:03 +0100

    types: add the pcl related configuration types

--- orb_slam2Types.hpp
+++ orb_slam2Types.hpp
@@ -9,6 +9,7 @@
  */
 
 #include <base/Time.hpp>
+#include <base/Eigen.hpp>
 
 namespace orb_slam2
 {
@@ -27,6 +28,39 @@ namespace orb_slam2
         int number_loops;
         float fps;
     };
+}
+
+namespace pituki
+{
+    enum OutlierFilterType
+    {
+        NONE,
+        STATISTICAL,
+        RADIUS
+    };
+
+    struct OutlierRemovalFilterConfiguration
+    {
+        OutlierFilterType type;
+
+        //STATISTICAL: the number of nearest neighbors to use for mean distance estimation (nr_k)
+        //RADIUS: Get the radius of the sphere that will determine which points are neighbors (radiu).
+        float parameter_one;
+
+        //STATISTICAL: the standard deviation multiplier for the distance threshold calculation.(stddev_null)
+        //RADIUS: number of neighbors that need to be present in order to be classified as an inlier(min_pts)
+        float parameter_two;
+    };
+
+    struct ConditionalRemovalConfiguration
+    {
+        bool filter_on;
+        bool keep_organized;
+        base::Vector3d gt_boundary;
+        base::Vector3d lt_boundary;
+    };
+
+
 }
 
 #endif
